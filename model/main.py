b4620a5b397338a79b0d96134b76c8cf52127836
Ruth Johnson <ruthjohnson@holylogin05.rc.fas.harvard.edu>
2024-12-12 13:42:16 -0500

    adding tutorial dir

--- model/main.py
+++ model/main.py
@@ -7,7 +7,6 @@ import pytorch_lightning as pl
 import random
 from model import EdgePredModel
 from dataloader import edge_pred_dataloader
-#from original import EdgePredModel, edge_pred_dataloader
 
 # set seeds
 torch.manual_seed(0)
@@ -35,40 +34,13 @@ def main():
     #
     # Set up model params
     #
-    #homo_hg = dgl.load_graphs("new_homo_hg.pt") 
+
+    # REPLACE WITH LOCAL VERSION OF KG HERE
     homo_hg = dgl.load_graphs("/n/home01/ruthjohnson/kg_paper/construct_kg/phekg/new_homo_hg_hms.pt")
     homo_hg = homo_hg[0][0]
     
     n_epochs=5
     wandb_logger = WandbLogger()
-
-    """
-    trainer = pl.Trainer(max_epochs=n_epochs,
-                        log_every_n_steps=1,
-                        precision="bf16-mixed",
-                        accelerator='gpu',
-                        logger=wandb_logger)
-
-    data_module = edge_pred_dataloader(homo_hg=homo_hg, 
-                                    e_batch_size=500, 
-                                    e_sampler=dgl.dataloading.MultiLayerNeighborSampler(
-                                        [100, 10], prob='p'), e_n_neg=10)
-
-    in_features = 1024
-    hidden_features_1 = 512
-    out_features = 128
-
-    model = EdgePredModel(homo_hg, in_features, hidden_features_1, out_features)
-
-    trainer.fit(model=model, datamodule=data_module)
-
-    torch.save(model.het_gnn.state_dict(), "hgt_model_mini_edge.pt")
-
-    """
-    # {'train_loop_config': {'num_layers': 4, 'n_neg': 3, 
-    # 'batch_size': 500, 'sampler_n': 15, 'lr': 0.0001, 
-    # 'in_feat': 128, 'out_feat': 128, 'head_size': 512, 
-    # 'num_heads': 3, 'dropout': 0}}
 
     config = {
         'num_layers': 4,
@@ -97,7 +69,7 @@ def main():
     data_module = edge_pred_dataloader(homo_hg=homo_hg, homo_hg_dict=config)
 
     model = EdgePredModel(homo_hg, hgt_config=config)
-    print(model)
+    
     # train!
     trainer.fit(model=model, datamodule=data_module)
 
@@ -109,7 +81,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# srun python  ...
 
